0d534c53e8c90aeb760fd087aab17053fcf1409d
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-08-26 03:57:30 +0000

    add 4568
    git-svn-id: svn://192.168.0.99/blogprogs@7430 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ 4568/testStepFilter/testStepFilter/testStepFilter.cpp
@@ -0,0 +1,38 @@
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+
+// Create "C:\Users\YourAccoutName\My Documents\Visual Studio 2017\Visualizers\.natstepfilter" with below content.
+/***********
+<?xml version="1.0" encoding="utf-8"?>
+<StepFilter xmlns="http://schemas.microsoft.com/vstudio/debugger/natstepfilter/2010">
+
+<Function><Name>operator new</Name><Action>NoStepInto</Action></Function>
+<Function><Name>operator delete</Name><Action>NoStepInto</Action></Function>
+<!-- Skip everything in std -->
+<Function><Name>std::.*</Name><Action>NoStepInto</Action></Function>
+<!-- all methods on WebKit OwnPtr and variants, ... WTF::*Ptr<*>::* -->
+<!-- <Function><Name>WTF::.*Ptr&lt;.*&gt;::.*</Name><Action>NoStepInto</Action></Function> -->
+
+</StepFilter>
+************/
+
+
+int main()
+{
+	// if the step filter is enabled, stepping into string ctor will be disabled.
+	string s;
+	s = "aaa";
+	s.append("bbb");
+
+	cout << s << endl;
+
+    return 0;
+}
+
