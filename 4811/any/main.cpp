0b38053df273bac8aa531ed65ca3d4f436b2b21f
kineko <kineko@10-Anniv>
2018-02-23 12:41:52 +0900

    4811

--- 4811/any/main.cpp
+++ 4811/any/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <memory>
 
 #if 1
 #include <boost/any.hpp>
@@ -12,28 +13,62 @@ using std::any;
 using std::any_cast;
 using std::bad_any_cast;
 #endif
+
+class C
+{
+public:
+    C()
+    {
+        std::cout << "C" << std::endl;
+    }
+    ~C()
+    {
+        std::cout << "~C" << std::endl;
+    }
+    void myok()
+    {
+        std::cout << "ok" << std::endl;
+    }
+};
+
 // Modified http://faithandbrave.hateblo.jp/entry/2016/07/29/181945
 int main()
 {
-  any a = 3; // int値を代入
-  a = std::string("hello"); // stringオブジェクトを代入
+    {
+        any ac = new C;
+    }
 
-  // 中身を取り出す
-  // 取り出せなかったらstd::bad_any_cast例外
-  try {
-    std::string s = any_cast<std::string>(a);
-    std::cout << s << std::endl;
-  }
-  catch (bad_any_cast& e) {
-    std::cout << "bad_any_cast: " << e.what() << std::endl;
-  }
+    {
+        std::shared_ptr<C> sp(new C);
+        sp->myok();
 
-  // 中身を取り出す
-  // こちらは取り出せなかったらヌルポインタが返る
-  if (std::string* s = any_cast<std::string>(&a)) {
-    std::cout << *s << std::endl;
-  }
-  else {
-    std::cout << "null" << std::endl;
-  }
+        any sharedac = std::shared_ptr<C>(new C);
+
+        // bad cast
+        // C* cp = any_cast<C*>(sharedac);
+        // cp->ok();
+
+        std::shared_ptr<C> spc = any_cast<std::shared_ptr<C>>(sharedac);
+        spc->myok();
+    }
+    any a = 3;
+    a = std::string("hello");
+
+    try
+    {
+        std::string s = any_cast<std::string>(a);
+        std::cout << s << std::endl;
+    }
+    catch (bad_any_cast& e)
+    {
+        std::cout << "bad_any_cast: " << e.what() << std::endl;
+    }
+
+    if (std::string* s = any_cast<std::string>(&a))
+    {
+        std::cout << *s << std::endl;
+    }
+    else {
+        std::cout << "null" << std::endl;
+    }
 }
