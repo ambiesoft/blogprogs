dc1145d663bfc5bfdd45fb86765ddc852c352ae4
fjUnc <fjUnc@THEXP>
2018-04-13 21:17:06 +0900

    add 4872

---
+++ 4872/qdtc/main.cpp
@@ -0,0 +1,27 @@
+#include <QCoreApplication>
+#include <QDateTime>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QDateTime dt1 = QDateTime::currentDateTime();
+    QDateTime dt2 = QDateTime::currentDateTimeUtc();
+
+    qDebug() << dt1;
+    qDebug() << dt2;
+
+    qDebug() << dt1.currentMSecsSinceEpoch();
+    qDebug() << dt2.currentMSecsSinceEpoch();
+
+    QDateTime outdt1 = QDateTime::fromMSecsSinceEpoch(dt1.toMSecsSinceEpoch());
+    QDateTime outdt2 = QDateTime::fromMSecsSinceEpoch(dt2.toMSecsSinceEpoch());
+
+    qDebug() << outdt1.currentMSecsSinceEpoch();
+    qDebug() << outdt2.currentMSecsSinceEpoch();
+
+    qDebug() << (outdt1==dt1);
+    qDebug() << (outdt2==dt2);
+    return a.exec();
+}
