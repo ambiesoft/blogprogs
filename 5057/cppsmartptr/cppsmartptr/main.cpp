36d2cfce74bf6f209da9a7a8da8c01765eea6aa1
ambiesoft <ambiesoft.trueff@gmail.com>
2018-08-17 16:53:42 +0900

    add main

--- 5057/cppsmartptr/cppsmartptr/main.cpp
+++ 5057/cppsmartptr/cppsmartptr/main.cpp
@@ -5,6 +5,30 @@ using namespace std;
 
 int main()
 {
+	{
+		unique_ptr<char> p;
+		if (p)
+			cout << "true" << endl;
+		if (!p)
+			cout << "false" << endl;
+
+		if(p.get())
+			cout << "true" << endl;
+
+		unique_ptr<char> q(new char);
+		if (q)
+			cout << "true" << endl;
+		if (!q)
+			cout << "false" << endl;
+
+		if (q.get())
+			cout << "true" << endl;
+
+		// q = p; // error
+		// unique_ptr<char> r(q); // error
+		// unique_ptr<char> r(q.get()); // not an error, but never do this
+		auto r = make_unique<char>('a');
+	}
     unique_ptr<char> p(new char);
     p.reset(new char);
     return 0;
