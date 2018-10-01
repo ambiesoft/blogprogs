855429782d319ae40ee8b6e4ac5f5a96569c1340
ambiesoft <ambiesoft.trueff@gmail.com>
2018-10-02 00:05:15 +0900

    remove some

--- 5135/src/stringtype/main.cpp
+++ 5135/src/stringtype/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <cstring>
+
 using namespace std;
 
-int main()
+void f1()
 {
     string cs = "aaa";
     string u8s = u8"aaa";
@@ -12,10 +15,32 @@ int main()
 
     cout << cs << endl;
     cout << u8s << endl;
-    cout << ws << endl;
-    cout << u16s << endl;
-    cout << u32s << endl;
+    wcout << ws << endl;
+}
 
-    cout << "Hello World!" << endl;
+void f2()
+{
+    const wchar_t* pwc = L"あいうえお";
+    char u8web[] =  {0xE3,0x81,0x82,0xE3,0x81,0x84,0xE3,0x81,0x86,0xE3,0x81,0x88,0xE3,0x81,0x8A,0x0};
+    const char* p8c = u8"あいうえお";
+    const char* pc = "あいうえお";
+
+    if(memcmp(u8web, p8c, strlen(u8web))==0)
+        cout << "equal" << endl;
+    else
+        cout << "not equal" << endl;
+
+    const char16_t* p16c = u"あいうえお";
+
+    if(memcmp(pwc, p16c, wcslen(pwc)*sizeof(wchar_t))==0)
+        cout << "equal" << endl;
+    else
+        cout << "not equal" << endl;
+}
+
+int main()
+{
+    f1();
+    f2();
     return 0;
 }
