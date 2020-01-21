8600556e435f9505ac6d852f06ec1d09934c71e6
ambiesoft <ambiesoft.trueff@gmail.com>
2020-01-22 00:16:26 +0900

    add

---
+++ 5623/txtvalidateandchange/txtvalidateandchange/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace txtvalidateandchange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(609, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            this.textBox1.Validated += new System.EventHandler(this.textBox1_Validated);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(88, 185);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(622, 215);
+            this.txtLog.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ 5623/txtvalidateandchange/txtvalidateandchange/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace txtvalidateandchange
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Text = "'z' is invalid";
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public string GetCurrentMethod()
+        {
+            var st = new StackTrace();
+            var sf = st.GetFrame(1);
+
+            return sf.GetMethod().Name + "(Text is '" + textBox1.Text + "')";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            txtLog.Text += (GetCurrentMethod());
+            txtLog.Text += "\r\n";
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            txtLog.Text += (GetCurrentMethod());
+            txtLog.Text += "\r\n";
+
+            if(textBox1.Text.IndexOf('z') >= 0)
+            {
+                MessageBox.Show("'z' is invalid");
+                e.Cancel = true;
+            }
+        }
+
+        private void textBox1_Validated(object sender, EventArgs e)
+        {
+            txtLog.Text += (GetCurrentMethod());
+            txtLog.Text += "\r\n";
+        }
+    }
+}
