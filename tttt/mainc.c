708ac817b7e0f13470640b40b5991b2a53c331d3
ambiesoft <ambiesoft.trueff@gmail.com>
2020-03-22 16:48:22 +0900

    hex

---
+++ tttt/mainc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	printf("The address of a is 0x%x.\n", &a);
+	return 0;
+}
+
---
+++ tttt/maincpp.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	int a;
+	std::cout << std::hex << &a << std::endl;
+	return 0;
+}
