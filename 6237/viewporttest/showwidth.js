fcc9267d6c0b5e1db2649ef88e7eaf2ca35199c9
ambiesoft <ambiesoft.trueff@gmail.com>
2021-09-14 02:52:54 +0900

    nazo

---
+++ 6237/viewporttest/showwidth.js
@@ -0,0 +1,17 @@
+
+function showwidth() {
+    let text =
+        `
+        screen.width=${screen.width}
+        screen.height=${screen.height}
+        document.documentElement.clientWidth=${document.documentElement.clientWidth}
+        document.documentElement.clientheight=${document.documentElement.clientHeight}
+        window.innerWidth=${window.innerWidth}
+        window.innerheight=${window.innerHeight}
+`;
+    document.getElementById('content').innerText = text;
+}
+
+window.setInterval(() => {
+    showwidth();
+}, 1000);
